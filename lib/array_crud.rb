42c587d9fc7d399fc114d4baf8440db6e80b2482
Alex Marz <alexhmarz@gmail.com>
2020-10-12 19:33:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  student = ["wow", "Morty", "Summer", "arrays!"]
+  student = ["wow", "am", "Summer", "arrays!"]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  create_an_array[2]
 end
 
 def retrieve_first_element_from_array(array)
