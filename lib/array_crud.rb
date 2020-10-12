82af08f08eaba1c62ecdc194389395c4acf06502
Alex Marz <alexhmarz@gmail.com>
2020-10-12 19:26:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  student = ["Rick", "Morty", "Summer", "arrays!"]
+  student = ["wow", "Morty", "Summer", "arrays!"]
 end
 
 def add_element_to_end_of_array(array, element)
